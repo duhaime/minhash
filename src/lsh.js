b0f98490a6c9ee236459e5b8941643f3f29d70bc
Rémy MEJA <remymeja@free.fr>
2018-10-12 10:52:26 +0200

    Added Jaccard similarity score in the result

--- src/lsh.js
+++ src/lsh.js
@@ -4,44 +4,51 @@
 * Main class for indexing Minhash signatures
 **/
 
-var LshIndex = function(args) {
-  var args = args || {};
+var LshIndex = function (args) {
+  args = args || {};
   this.bandSize = args.bandSize || 4;
   this.index = {};
 
-  this.insert = function(key, minhash) {
+  this.insert = function (key, minhash) {
     var hashbands = this.getHashbands(minhash);
-    for (var i=0; i<hashbands.length; i++) {
+    for (var i = 0; i < hashbands.length; i++) {
       var band = hashbands[i];
       if (Array.isArray(this.index[band])) {
         this.index[band].push(key);
       } else {
         this.index[band] = [key];
       }
-    };
+    }
   };
 
-  this.query = function(minhash) {
+  this.query = function (minhash) {
     var matches = {};
     var hashbands = this.getHashbands(minhash);
-    for (var i=0; i<hashbands.length; i++) {
+    for (var i = 0; i < hashbands.length; i++) {
       var band = hashbands[i];
-      for (var j=0; j<this.index[band].length; j++) {
-        matches[this.index[band][j]] = true;
-      };
-    };
-    return Object.keys(matches);
+      if (this.index.hasOwnProperty(band)) {
+        for (let j = 0; j < this.index[band].length; j++) {
+          if (!matches.hasOwnProperty([this.index[band][j]])) matches[this.index[band][j]] = { hits: 0, result: 0 };
+          matches[this.index[band][j]].hits++;
+          matches[this.index[band][j]].result = matches[this.index[band][j]].hits / hashbands.length;
+        }
+      }
+    }
+    return Object.keys(matches).map(match => {
+      matches[match].name = match;
+      return matches[match];
+    });
   };
 
-  this.getHashbands = function(minhash) {
+  this.getHashbands = function (minhash) {
     if (minhash.hashbands) return minhash.hashbands;
     minhash.hashbands = [];
-    for (var i=0; i<minhash.hashvalues.length / this.bandSize; i++) {
+    for (var i = 0; i < minhash.hashvalues.length / this.bandSize; i++) {
       var start = i * this.bandSize;
       var end = start + this.bandSize;
       var band = minhash.hashvalues.slice(start, end);
       minhash.hashbands.push(band.join('.'));
-    };
+    }
     return minhash.hashbands;
   };
 };
